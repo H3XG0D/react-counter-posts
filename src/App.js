cec284db4317e2ae27b557ef96163c858295f4f3
H3XG0D <npc.legit@gmail.com>
2023-03-02 23:31:47 +0900

    again commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'This language is bad...'},
+        {id: 2, title: 'Python', body: 'This is best language!'},
+        {id: 3, title: 'Golang', body: 'I dont know that shit.'}
+    ])
+
+    const [posts2, setPosts2] = useState([
+        {id: 1, title: 'C#', body: 'This language is bad...'},
+        {id: 2, title: 'C++', body: 'This is best language!'},
+        {id: 3, title: 'Java', body: 'I dont know that shit.'}
+    ])
+
+    return (
+        <div className="App">
+            <PostList posts={posts} title="Part 1. Language"/>
+            <PostList posts={posts2} title="Part 2. Other language"/>
+        </div>
+    );
 }
 
 export default App;
